e11ec58f64fc0d2a96ae950ee0c7f23847b8c9ec
Liword38 <matnico@laposte.net>
2020-01-15 18:48:14 +0100

    Add Update album

--- src/main/java/fr/uga/miashs/sempic/backingbeans/EditAlbum.java
+++ src/main/java/fr/uga/miashs/sempic/backingbeans/EditAlbum.java
@@ -7,7 +7,13 @@ package fr.uga.miashs.sempic.backingbeans;
 
 import fr.uga.miashs.sempic.SempicModelException;
 import fr.uga.miashs.sempic.dao.AlbumFacade;
+import fr.uga.miashs.sempic.dao.GroupFacade;
+import fr.uga.miashs.sempic.dao.SempicUserFacade;
 import fr.uga.miashs.sempic.entities.SempicAlbum;
+import fr.uga.miashs.sempic.entities.SempicGroup;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
@@ -21,44 +27,59 @@ import javax.inject.Named;
  */
 @Named
 @RequestScoped
-public class EditAlbum {
-    
+public class EditAlbum implements Serializable {
+
     private SempicAlbum target;
-    
+
     @Inject
     private AlbumFacade albumDao;
-    
+    @Inject
+    private SempicUserFacade userDao;
+    @Inject
+    private GroupFacade groupDao;
+
+
     public EditAlbum() {
-        
+
     }
-    
+
     @PostConstruct
     public void init() {
         target = new SempicAlbum();
     }
- 
-    
+
+    public void setOwnerId(String id) {
+        System.out.println(id);
+        target.setAlbumOwner(userDao.read(Long.valueOf(id)));
+    }
+
+    public String getOwnerId() {
+        if (target.getAlbumOwner() == null) {
+            return "-1";
+        }
+        return "" + target.getAlbumOwner().getId();
+    }
+
     public SempicAlbum getTarget() {
         return target;
     }
-    
+
     public void setTarget(SempicAlbum target) {
         this.target = target;
     }
-    
+
     public String update() {
-        
+
         try {
-            albumDao.update(target);
-        
-        }
-        catch (SempicModelException ex) {
-           FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(ex.getMessage()));
-            return "failure";
             
+            albumDao.update(target);
+
+        } catch (SempicModelException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(ex.getMessage()));
+            return "failure";
+
         }
         return "success";
     }
-    
-    
+
 }
